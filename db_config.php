7e04608132f0d4a30d00f891f6986ba6315706fa
saharat phothongkom <saharat.phothongkom@krungsri.com>
2023-05-19 15:29:29 +0700

    Bank | update dataclass

--- db_config.php
+++ db_config.php
@@ -1,12 +1,12 @@
 <?php   
 date_default_timezone_set("Asia/Bangkok");
 
-ini_set('display_errors', 1);
-error_reporting(~0);
-error_reporting (E_ALL ^ E_NOTICE);
-// ini_set('display_errors', '1');
-// ini_set('display_startup_errors', '1');
-// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// error_reporting(~0);
+// error_reporting (E_ALL ^ E_NOTICE);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
 
 	  
