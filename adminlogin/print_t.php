f4fa705bc3e8eb39f8f5086bf34c68b9e77ced7f
saharat phothongkom <saharat.phothongkom@krungsri.com>
2023-08-23 04:58:12 +0700

    req update

--- adminlogin/print_t.php
+++ adminlogin/print_t.php
@@ -16,7 +16,7 @@ $master = $query->fetch_assoc();
 // create new PDF document
 // $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-$pdf = new TCPDF('P', 'cm', 'A4', true, 'UTF-8', false);
+$pdf = new TCPDF('P',   'cm', 'A4', true, 'UTF-8', false);
 
 $pdf->setPrintFooter(false);
 $pdf->SetMargins(1, 1, 1);
